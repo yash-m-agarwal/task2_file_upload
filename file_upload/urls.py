d76145979232ac9cf22be4e0f9ee2261b8adad2f
yash-m-agarwal <yashmagarwal786@gmail.com>
2019-03-31 09:34:57 +0530

    UI enhanced

--- file_upload/urls.py
+++ file_upload/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from files import views
 
@@ -24,3 +26,6 @@ urlpatterns = [
     url(r'file/new/$', views.new_upload, name='new_upload'),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
